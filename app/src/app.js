a68e62410ef0701500405a232f11ca360c9a993a
elodiebatista <elodie.batista@live.fr>
2013-12-23 23:29:20 +0100

    Fixing anchors links (temp, I know...)

--- app/src/app.js
+++ app/src/app.js
@@ -14,10 +14,15 @@ app.config(function($locationProvider, $httpProvider, $routeProvider, $sceDelega
  * App Run block: get executed after the injector is created and are used to kickstart the application
  * Only instances and constants can be injected into run blocks. This is to prevent further system configuration during application run time
  */
-app.run(function($rootScope) {
+app.run(function($rootScope, $location, $anchorScroll) {
     $rootScope.srvEndpoint = '';
 
     $rootScope.$on('$routeChangeStart', function() {
         console.log('Route change start');
     });
+
+    $rootScope.scrollTo = function(id) {
+        $location.hash(id);
+        $anchorScroll();
+    }
 });
