9c2150abec3545a3dd016b37b03323abe0c4beaf
elodiebatista <elodie.batista@live.fr>
2014-01-06 14:57:21 +0100

    Never mind --" Adding jqTree
    http://mbraak.github.io/jqTree/

--- app/src/files/files.js
+++ app/src/files/files.js
@@ -16,71 +16,51 @@ module.controller('FilesCtrl',
         console.log('Files Controller');
         $scope.test = 'test';
 
-        var json_from_api = {
-            id: 'root',
-            next: [
+        var json_from_api = [
                 {
-                    id: 'id_10',
                     type: 'folder',
                     name: 'America',
-                    next: [
+                    children: [
                         {
-                            id: 'id_11',
                             type: 'folder',
-                            name: 'USA',
-                            previous: 'id_10'
+                            name: 'USA'
                         },
                         {
-                            id: 'id_12',
                             type: 'folder',
-                            name: 'Canada',
-                            previous: 'id_10'
+                            name: 'Canada'
                         }
                     ]
                 },
                 {
-                    id: 'id_20',
                     type: 'folder',
                     name: 'Europe',
-                    next: [
+                    children: [
                         {
-                            id: 'id_21',
                             type: 'folder',
-                            name: 'France',
-                            previous: 'id_20'
+                            name: 'France'
                         },
                         {
-                            id: 'id_22',
                             type: 'folder',
-                            name: 'Portugal',
-                            previous: 'id_20'
+                            name: 'Portugal'
                         },
                         {
-                            id: 'id_23',
                             type: 'folder',
-                            name: 'Spain',
-                            previous: 'id_20'
+                            name: 'Spain'
                         }
                     ]
                 },
                 {
-                    id: 'id_30',
                     type: 'folder',
                     name: 'Oceania',
-                    next: [
+                    children: [
                         {
-                            id: 'id_31',
                             type: 'folder',
-                            name: 'Australia',
-                            previous: 'id_30'
+                            name: 'Australia'
                         }
                     ]
                 }
-            ]
-        };
+            ];
 
         $scope.files = json_from_api;
-
-
     }
 );
--- app/src/files/treeView.js
+++ app/src/files/treeView.js
@@ -5,79 +5,19 @@ var module = angular.module('webApp');
 /**
  * Defines the way to display a tree view
  */
-module.directive('treeView', function($compile) {
+module.directive('treeView', function() {
     return {
         restrict: 'E',
         scope: '{}',
-        templateUrl: '/src/files/treeView.tpl.html',
 
         link: function (scope, element, attrs) {
             scope.$watch(attrs.items, function(newValue, oldValue) {
                 scope.items = newValue;
-                scope.id_selected = scope.items.next[0].id;
-                bfs(scope.items);
+
+                element.tree({
+                    data: scope.items
+                });
             });
-
-            function bfs(sourceNode) {
-                if (sourceNode === null) { return null; }
-
-                var queue = [];
-                queue.push(sourceNode);
-
-                while (queue.length !== 0) {
-                    var node = queue.shift();
-
-                    if (node.previous) {
-                        if (node.next) {
-                            $('#treeview #' + node.previous + ' > ul').append($compile(
-                                '<li id="' + node.id + '">' +
-                                    '<span class="tree-view-item-selectable" ng-click="select(\'' + node.id + '\')" ng-class="itemClass(\'' + node.id + '\')">' + node.name + '</span> ' +
-                                    '<ul></ul>' +
-                                '</li>'
-                            )(scope));
-                        } else {
-                            $('#treeview #' + node.previous + ' > ul').append($compile(
-                                '<li id="' + node.id + '">' +
-                                    '<span>' + node.name + '</span>' +
-                                '</li>'
-                            )(scope));
-                        }
-                    } else {
-                        if (node.id !== 'root') {
-                            if (node.next) {
-                                $('#treeview ul.tree-view').append($compile(
-                                    '<li id="' + node.id + '">' +
-                                        '<span class="tree-view-item-selectable" ng-click="select(\'' + node.id + '\')" ng-class="itemClass(\'' + node.id + '\')">' + node.name + '</span> ' +
-                                        '<ul></ul>' +
-                                    '</li>'
-                                )(scope));
-                            } else {
-                                $('#treeview ul.tree-view').append($compile(
-                                    '<li id="' + node.id + '">' +
-                                        '<span class="tree-view-item-selectable" ng-click="select(\'' + node.id + '\')" ng-class="itemClass(\'' + node.id + '\')">' + node.name + '</span>' +
-                                    '</li>'
-                                )(scope));
-                            }
-                        }
-                    }
-
-                    if (node.next) {
-                        for (var i = 0, l = node.next.length; i < l; i++) {
-                            queue.push(node.next[i]);
-                        }
-                    }
-                }
-                return null;
-            }
-
-
-            scope.select = function(id_item) {
-                scope.id_selected = id_item;
-            };
-
-            scope.itemClass = function(id_item) {
-                return id_item === scope.id_selected ? 'tree-view-item-selected' : undefined;
-            };
         }
     };
 });
