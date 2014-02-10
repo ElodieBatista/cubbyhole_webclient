cb4f11ee6b2ca35492b9e62ec846a30889150577
elodiebatista <elodie.batista@live.fr>
2014-02-10 22:28:21 +0100

    Adding sign up jQuery ajax POST request

--- app/src/lib/custom/index-init.js
+++ app/src/lib/custom/index-init.js
@@ -19,7 +19,7 @@ $('#signin form').submit(function(event) {
     var pass = $(this).find('input')[1].value;
     var remember = $(this).find('input')[2].checked;
 
-    console.log('Sending login info: ' + email + ' ' + pass);
+    console.log('Sending sign in info: ' + email + ' ' + pass);
 
     $.ajax({
         type: 'POST',
@@ -29,15 +29,56 @@ $('#signin form').submit(function(event) {
             pass: pass
         }
     }).done(function(data) {
-        console.log('Login success');
+        console.log('Sign in success');
 
-        if (remember) {
-            localStorage.setItem('key', key);
-            localStorage.setItem('profile', JSON.stringify(data.profile));
+        localStorage.setItem('key', data.key);
+        localStorage.setItem('profile', JSON.stringify(data.profile));
+        localStorage.setItem('remember', remember);
 
-            console.log('Saving Auth info in the LocalStorage: ' + data.key + ' ' + data.profile.toString());
-        }
+        console.log('Saving Auth info in the LocalStorage: ' + data.key + ' ' + data.profile.toString());
     }).fail(function(error) {
-        console.log('Login failed: ' + error.msg);
+        console.log('Sign in failed: ' + error.msg);
+    });
+});
+
+// Sign Up
+$('#signup form').submit(function(event) {
+    event.preventDefault();
+
+    var pass = $(this).find('input')[1].value;
+    var pass2 = $(this).find('input')[2].value;
+
+    if (pass !== pass2) {
+        alert('Passwords don\'t match');
+        $(this).find('input[type="password"]').parent().css('background-color', 'rgb(255, 80, 81)');
+        setTimeout(function() {
+            $('#signup form').find('input[type="password"]').parent().css('background-color', 'white');
+        }, 1000);
+        return;
+    }
+
+    var email = $(this).find('input')[0].value;
+    var plan = $(this).find('select')[0].value;
+
+    console.log('Sending sign up info: ' + email + ' ' + pass + ' ' + plan);
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:9000/signup',
+        data: {
+            email: email,
+            pass: pass,
+            plan: plan
+        }
+    }).done(function(data) {
+        console.log('Sign up success');
+
+        localStorage.setItem('key', data.key);
+        localStorage.setItem('profile', JSON.stringify(data.profile));
+        localStorage.setItem('remember', false);
+
+        console.log('Saving Auth info in the LocalStorage: ' + data.key + ' ' + data.profile.toString());
+    }).fail(function(error) {
+        console.log('Sign up failed: ' + error.msg);
     });
 });
