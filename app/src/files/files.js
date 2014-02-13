d96320d37bdfa884bce51fe26c241a2a8d383a0a
elodiebatista <elodie.batista@live.fr>
2014-02-13 22:27:34 +0100

    Preparing drop to upload

--- app/src/files/files.js
+++ app/src/files/files.js
@@ -14,11 +14,24 @@ module.config(function config($routeProvider) {
 });
 
 module.controller('FilesCtrl',
-    function FilesCtrl($rootScope, $scope, $routeParams) {
+    function FilesCtrl($rootScope, $scope, $routeParams, $resource, $upload) {
         // Highlight first btn in the nav bar
         $rootScope.navtop = 0;
 
         // TODO: Request all files to API
+        var Files = $resource($rootScope.srvEndpoint + 'item', {}, {
+            'get': {
+                method: 'GET'
+            },
+            'post': {
+                method:'POST',
+                params: {
+                    type:'@type',
+                    content:'@content'
+                }
+            }
+        });
+
 
         var json_from_api = [
             {
@@ -208,6 +221,13 @@ module.controller('FilesCtrl',
             $scope.$apply();
         }, 2000);
 
+        /*Files.get(function(res) {
+            $scope.folders = res.items;
+            $scope.$apply();
+        }, function(err) {
+            console.log('Can\'t get the files.');
+        });*/
+
         $scope.path = $routeParams.path;
 
         $scope.newFolder = function(form) {
@@ -217,6 +237,37 @@ module.controller('FilesCtrl',
 
                 $scope.addFolder(form.name);
             }
+        };
+
+        $scope.onFileSelect = function($files) {
+            //$files: an array of files selected, each file has name, size, and type.
+            //for (var i = 0; i < $files.length; i++) {
+                //var file = $files[i];
+            $scope.uploading = true;
+                $scope.upload = $upload.upload({
+                    url: $rootScope.srvEndpoint + 'item', //upload.php script, node.js route, or servlet url
+                    method: 'POST',
+                    // headers: {'headerKey': 'headerValue'},
+                    data: {myObj: $scope.myModelObj},
+                    file: $files //upload multiple files, this feature only works in HTML5 FromData browsers
+                    // file: file,
+                    /* set file formData name for 'Content-Desposition' header. Default: 'file' */
+                    //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+                    /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
+                    //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
+                }).progress(function(evt) {
+                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function(data, status, headers, config) {
+                    // file is uploaded successfully
+                    console.log(data);
+                    $scope.uploading = false;
+                })
+                .error(function(err) {
+                    console.log(err);
+                    $scope.uploading = false;
+                });
+                //.then(success, error, progress);
+            //}
         };
     }
 );
