1d242f056e83654394a9844e79a26eda47230659
elodiebatista <elodie.batista@live.fr>
2014-03-12 23:48:13 +0100

    User profile page clean

--- app/src/user/user.js
+++ app/src/user/user.js
@@ -17,6 +17,16 @@ module.controller('UserCtrl',
     // Highlight first btn in the nav bar
     $rootScope.navtop = 4;
 
+    $scope.getColorClass = function(percent) {
+      if (percent < 50) {
+        return 'quaternary-btn';
+      } else if (percent >= 50 && percent < 75) {
+        return 'tertiary-btn';
+      } else {
+        return 'primary-btn';
+      }
+    };
+
     var User = $resource(conf.epApi + '/user/:id', {id:$rootScope.getProfile().id}, {
       'get': {
         method: 'GET'
@@ -24,15 +34,33 @@ module.controller('UserCtrl',
     });
 
     User.get(function(res) {
-      console.log(res);
       $scope.user = res.data;
-      /*$scope.user = {
-        email: $rootScope.getProfile().email,
-        registrationDate: '2014-03-06T08:51:42.928Z',
-        currentPlan: 'Free'
-      };*/
-    }, function(error) {
-      console.log('Can\'t get the notifications.');
+
+      // TEMP
+      $scope.user.currentPlan = {};
+      $scope.user.usedStorage = 600;
+      $scope.user.currentPlan.storage = 1000;
+      $scope.user.usedSharedQuota = 200;
+      $scope.user.currentPlan.sharedQuota = 1000;
+      $scope.user.usedBandwidth = 750;
+      $scope.user.currentPlan.bandwidth = 1000;
+
+      $scope.stats = {
+        storage: ($scope.user.usedStorage * 100) / $scope.user.currentPlan.storage,
+        //storage: 60,
+        sharedQuota: ($scope.user.usedSharedQuota * 100) / $scope.user.currentPlan.sharedQuota,
+        //sharedQuota: 20,
+        bandwidth: ($scope.user.usedBandwidth * 100) / $scope.user.currentPlan.bandwidth
+        //bandwidth: 75
+      };
+
+      $scope.statsStyles = {
+        storage: $scope.getColorClass($scope.stats.storage),
+        sharedQuota: $scope.getColorClass($scope.stats.sharedQuota),
+        bandwidth: $scope.getColorClass($scope.stats.bandwidth)
+      }
+    }, function(err) {
+      console.log('Can\'t get the user.');
     });
   }
 );
