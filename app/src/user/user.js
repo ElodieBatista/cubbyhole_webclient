dccf3f45e272bbd0b60af3cd682e47f08e015f41
elodiebatista <elodie.batista@live.fr>
2014-03-13 18:36:33 +0100

    Dynamic Plans in user's profile page

--- app/src/user/user.js
+++ app/src/user/user.js
@@ -33,11 +33,20 @@ module.controller('UserCtrl',
       }
     });
 
+    var Plans = $resource(conf.epApi + '/plan', {}, {
+      'get': {
+        method: 'GET'
+      }
+    });
+
     User.get(function(res) {
       $scope.user = res.data;
 
       // TEMP
-      $scope.user.currentPlan = {};
+      $scope.user.currentPlan = {
+        _id: $scope.user.currentPlan,
+        name: 'Free'
+      };
       $scope.user.usedStorage = 600;
       $scope.user.currentPlan.storage = 1000;
       $scope.user.usedSharedQuota = 200;
@@ -58,9 +67,46 @@ module.controller('UserCtrl',
         storage: $scope.getColorClass($scope.stats.storage),
         sharedQuota: $scope.getColorClass($scope.stats.sharedQuota),
         bandwidth: $scope.getColorClass($scope.stats.bandwidth)
-      }
+      };
+
+      $scope.plans = [
+        {
+          _id: $scope.user.currentPlan._id,
+          name: 'free',
+          duration: 0,
+          storage: 100,
+          sharedQuota: 100,
+          bandwidth: 100,
+          price: 0
+        },
+        {
+          _id: 'abcdefghi123456',
+          name: 'pro',
+          duration: 90,
+          storage: 300,
+          sharedQuota: 300,
+          bandwidth: 300,
+          price: 14.99
+        },
+        {
+          _id: 'aaabbbccc111222',
+          name: 'business',
+          duration: 365,
+          storage: 1000,
+          sharedQuota: 1000,
+          bandwidth: 1000,
+          price: 24.99
+        }
+      ];
     }, function(err) {
       console.log('Can\'t get the user.');
     });
+
+
+    //Plans.get(function(res) {
+      //$scope.plans = res.data;
+    /*}, function(err) {
+      console.log('Can\'t get the plans.');
+    });*/
   }
 );
