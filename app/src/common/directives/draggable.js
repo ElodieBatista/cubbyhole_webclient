9f99ecde0f695991d8d0219823a1f8f2940f8aea
elodiebatista <elodie.batista@live.fr>
2014-03-15 12:20:24 +0100

    Adding permissions check in actionBar

--- app/src/common/directives/draggable.js
+++ app/src/common/directives/draggable.js
@@ -9,19 +9,24 @@ module.directive('draggable', function() {
   return {
     restrict: 'A',
     scope: {
-      item: '=item'
+      item: '=item',
+      candrag: '=candrag'
     },
 
     link: function (scope, element, attrs) {
       var el = element[0];
 
-      el.draggable = true;
+      if (scope.candrag) {
+        el.draggable = true;
+      }
 
       el.addEventListener(
         'dragstart',
         function(e) {
-          e.dataTransfer.setData('item', scope.item._id);
-          this.parentNode.classList.add('dnd-drag');
+          if (scope.candrag) {
+            e.dataTransfer.setData('item', scope.item._id);
+            this.parentNode.classList.add('dnd-drag');
+          }
           return false;
         },
         false
