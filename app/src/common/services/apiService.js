f4a70ddaa1a5250d09855cd25ba452cb57e29fb0
elodiebatista <elodie.batista@live.fr>
2014-03-29 14:21:05 +0100

    Adding errors msgs

--- app/src/common/services/apiService.js
+++ app/src/common/services/apiService.js
@@ -17,7 +17,6 @@ module.factory('apiService', function(conf, $resource) {
         }
       }
     }),
-
     Item: $resource(conf.epApi + '/item/:id', {id:'@id'}, {
       'get': {
         method:'GET'
@@ -39,6 +38,24 @@ module.factory('apiService', function(conf, $resource) {
         method:'DELETE'
       }
     }),
+    ItemError: {
+      GET: {
+        403: 'You are not allowed to access this item.',
+        404: 'This item doesn\'t exist.'
+      },
+      POST: {
+        403: 'You don\'t have permissions to create this item.',
+        404: 'This item doesn\'t exist.',
+        422: 'This item\'s parent doesn\'t exist.'
+      },
+      PUT: {
+        404: 'This item doesn\'t exist.'
+      },
+      DELETE: {
+        403: 'You are not allowed to delete this item.',
+        404: 'This item doesn\'t exist.'
+      }
+    },
 
 
     Shares: $resource(conf.epApi + '/share', {}, {
@@ -46,7 +63,6 @@ module.factory('apiService', function(conf, $resource) {
         method:'GET'
       }
     }),
-
     Share: $resource(conf.epApi + '/share/:id', {id:'@id'}, {
       'post': {
         method:'POST',
@@ -65,12 +81,24 @@ module.factory('apiService', function(conf, $resource) {
         method:'DELETE'
       }
     }),
-
     ShareRevoke: $resource(conf.epApi + '/share/:id/:member', {id:'@id', member:'@member'}, {
       'delete': { // If member, revoke permission
         method:'DELETE'
       }
     }),
+    ShareError: {
+      POST: {
+        403: 'You don\'t have permissions to access this item.',
+        404: 'This item doesn\'t exist.',
+        422: 'One or several of the members you entered are not Cubbyhole users.'
+      },
+      PUT: {
+        404: 'This item doesn\'t exist.'
+      },
+      DELETE: {
+        404: 'This item doesn\'t exist.'
+      }
+    },
 
 
     Links: $resource(conf.epApi + '/link', {}, {
@@ -78,7 +106,6 @@ module.factory('apiService', function(conf, $resource) {
         method: 'GET'
       }
     }),
-
     Link: $resource(conf.epApi + '/link/:id', {id:'@id'}, {
       'post': {
         method:'POST'
@@ -93,6 +120,18 @@ module.factory('apiService', function(conf, $resource) {
         method:'DELETE'
       }
     }),
+    LinkError: {
+      POST: {
+        403: 'You don\'t have permissions to access this item.',
+        404: 'This item doesn\'t exist.'
+      },
+      PUT: {
+        404: 'This item doesn\'t exist.'
+      },
+      DELETE: {
+        404: 'This item doesn\'t exist.'
+      }
+    },
 
 
     Notifications: $resource(conf.epApi + '/notification', {}, {
@@ -100,12 +139,17 @@ module.factory('apiService', function(conf, $resource) {
         method: 'GET'
       }
     }),
-
     Notification: $resource(conf.epApi + '/notification/:id', {}, {
       'delete': {
         method: 'DELETE'
       }
     }),
+    NotificationError: {
+      DELETE: {
+        403: 'You\'re not allowed to delete this notification.',
+        404: 'This notification doesn\'t exist.'
+      }
+    },
 
 
     Users: $resource(conf.epApi + '/user/', {}, {
@@ -113,19 +157,34 @@ module.factory('apiService', function(conf, $resource) {
         method: 'DELETE'
       }
     }),
-
-
     User: $resource(conf.epApi + '/user/:id', {id:'@id'}, {
       'get': {
         method: 'GET'
       }
     }),
+    UserError: {
+      GET: {
+        404: 'This user doesn\'t exist.'
+      },
+      PUT: {
+        404: 'This user doesn\'t exist.'
+      }
+    },
 
 
     Plans: $resource(conf.epApi + '/plan', {}, {
       'get': {
         method: 'GET'
       }
-    })
+    }),
+    PlanError: {
+      GET: {
+        404: 'This plan doesn\'t exist.'
+      },
+      DELETE: {
+        403: 'This plan can\'t be deleted.',
+        404: 'This plan doesn\'t exist.'
+      }
+    }
   };
 });
